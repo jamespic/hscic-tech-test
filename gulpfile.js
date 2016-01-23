1c6c0bd03052dfc29520d3c6618ccd0765898f04
James Pickering <james_pic@hotmail.com>
2016-01-23 16:59:45 +0000

    Initial commit - does nothing, but Flowtype/Babel/Gulp are set up

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var flow = require('gulp-flowtype');
+ 
+gulp.task('typecheck', function() {
+  return gulp.src('./src/*.js')
+    .pipe(flow({
+        all: false,
+        weak: false,
+        declarations: './declarations',
+        killFlow: false,
+        beep: false,
+        abort: false
+    }))
+    .pipe(babel())
+    .pipe(gulp.dest('./lib'));
+});
---
+++ src/search.js
@@ -0,0 +1,3 @@
+/* @flow */
+
+var readline = require("readline")
